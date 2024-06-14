d7ca1d3b20afac91c2641ebe73f9d9633922d7fb
rei <rei@localhost.localdomain>
2024-06-14 12:08:55 -0500

    refactory texts

--- products/forms.py
+++ products/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 
 from .models import Product
+from categories.models import Category
 
 class ProductForm(forms.ModelForm):
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Seleccionar categoría")
+    status = forms.ChoiceField(choices=[('', 'Seleccionar estado'),('Available', 'Disponible'), ('Disabled', 'No Disponible')])
     class Meta:
         model = Product
         fields = ['code', 'name', 'category', 'status', 'stock', 'price']
--- stock_reception/forms.py
+++ stock_reception/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 
 from .models import StockReception
+from products.models import Product
+from suppliers.models import Supplier
 
 class StockReceptionForm(forms.ModelForm):
+    product = forms.ModelChoiceField(queryset=Product.objects.all(), empty_label="Seleccionar producto")
+    supplier = forms.ModelChoiceField(queryset=Supplier.objects.all(), empty_label="Seleccionar proveedor")
     class Meta:
         model = StockReception
         fields = ['quantity_received', 'unit_price', 'reception_date', 'note', 'product', 'supplier']
